2315d791f408b9cdd7b7d708f222d2befdafdd8e
yasser0-01 <ytcastelltortyasser@gmail.com>
2024-07-12 09:33:27 -1200

    Sample Commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function home(): Response
     {
         $data = $this->getDoctrine()->getRepository(Crud::class)->findAll();
