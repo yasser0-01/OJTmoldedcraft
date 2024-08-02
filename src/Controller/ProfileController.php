0938d0b54539022c9ac7b888db9f7440396b4831
yasser011 <ytcastelltortyasser@gmail.com>
2024-08-01 17:06:42 -1200

    august 1, 2024

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +13,22 @@ class ProfileController extends AbstractController
     public function pricing(): Response
     {
         return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    #[Route('/profilecompany', name: 'app_profilecompany')]
+    public function company(Request $request)
+    {
+        return $this->render('profile/company.html.twig',[
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    #[Route('/profileqr', name: 'app_profileqr')]
+    public function qr(Request $request)
+    {
+        return $this->render('profile/qr.html.twig',[
             'controller_name' => 'ProfileController',
         ]);
     }
